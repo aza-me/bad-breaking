45087ffabc679f09169a544690ff5e95852f50f1
CECK <az.ah.qwerty@gmail.com>
2022-10-09 17:10:48 +0500

    search router

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import CharactersPage from 'pages/CharactersPage/CharactersPage';
 import CurrentCharacter from 'pages/CurrentCharacter/CurrentCharacter';
 import EpisodeSeries from 'pages/EpisodePage/EpisodePage';
 import MainPage from 'pages/MainPage/MainPage';
+import SearchPage from 'pages/SearchPage/SearchPage';
 import React from 'react';
 import { Route, Routes, unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import history from './history';
@@ -16,6 +17,7 @@ const AppRoutes: React.FC = () => {
           <Route element={<EpisodeSeries />} path='/episodes/:id' />
           <Route element={<CharactersPage />} path='/characters' />
           <Route element={<CurrentCharacter />} path='/characters/:name' />
+          <Route element={<SearchPage />} path='/search' />
         </Routes>
       </App>
     </HistoryRouter>
