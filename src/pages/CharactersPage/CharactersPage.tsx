61dda9e1f98002676ddb9d1d55ca5ceaa1d48fe3
CECK <az.ah.qwerty@gmail.com>
2022-10-15 12:44:29 +0500

    death searcher

--- src/pages/CharactersPage/CharactersPage.tsx
+++ src/pages/CharactersPage/CharactersPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from 'store/hooks'
 import { GetAllDeaths } from 'store/modules/deaths'
@@ -6,13 +6,16 @@ import s from './CharactersPage.module.scss'
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import { Box, Container } from '@mui/system'
+import { deathsModel } from 'app/models/deaths'
 
 
 
 const CharactersPage: React.FC = () => {
+    const [input, setInput] = useState('')
+    const [searchedData, setSearchedData] = useState<deathsModel[]>([])
     const dispatch = useAppDispatch()
     const { deaths } = useAppSelector((state) => state.deaths)
-    
+
 
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -22,18 +25,29 @@ const CharactersPage: React.FC = () => {
         width: '100%',
         color: theme.palette.text.secondary,
     }));
+    const searchByName = (name: string) => {
+        setInput(name)
+        const searchedArr = deaths.filter(deaths => deaths.death.toLowerCase().includes(input.toLowerCase()))
+        setSearchedData(searchedArr)
 
+    }
+    
     useEffect(() => {
         dispatch(GetAllDeaths())
+        console.log(searchedData);
+        
     }, [])
-
     useEffect(() => {
+        console.log(searchedData);
         console.log(deaths);
     }, [deaths]);
     return (
         <Container >
+            <input value={input} onChange={e => searchByName(e.target.value)} type="text" name="" id="" />
             <Box sx={{ flexGrow: 1, display: 'flex', gap: '50px', flexDirection: 'column', flexWrap: 'nowrap', maxWidth: "100%" }}>
-                {deaths.map(e => <div key={e.death_id} className={s.item}>  <NavLink to={`/characters/${e.death.split(' ').splice(0, 1).join('+')}`} ><Item className={s.element}> {e.death}</Item> </NavLink> </div>)}
+                {input.length != 0 ? (searchedData.map(e => <div key={e.death_id} className={s.item}>  <NavLink to={`/characters/${e.death.split(' ').splice(0, 1).join('+')}`} ><Item className={s.element}> {e.death}</Item> </NavLink> </div>)) : (
+                    deaths.map(e => <div key={e.death_id} className={s.item}>  <NavLink to={`/characters/${e.death.split(' ').splice(0, 1).join('+')}`} ><Item className={s.element}> {e.death}</Item> </NavLink> </div>)
+                )}
             </Box>
         </Container>
     )
