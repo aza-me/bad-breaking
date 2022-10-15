8afb22f0ecc63ce3e62ea7e3370ec3e2fd580043
CECK <az.ah.qwerty@gmail.com>
2022-10-15 21:16:42 +0500

    site searcher

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,3 +1,5 @@
+import { deathsModel } from 'app/models/deaths'
+import { EpisodeModel } from 'app/models/episodes'
 import React, { useEffect, useState } from 'react'
 import { useAppDispatch, useAppSelector } from 'store/hooks'
 import { GetAllDeaths } from 'store/modules/deaths'
@@ -6,10 +8,11 @@ import s from './SearchPage.module.scss'
 
 const SearchPage: React.FC = () => {
     const [input, setInput] = useState('')
+    const [filtredDeath, setFiltredDeath] = useState<deathsModel[]>([])
+    const [filtredEpisodes, setFiltredEpisodes] = useState<EpisodeModel[]>([])
     const dispatch = useAppDispatch()
     const { deaths } = useAppSelector(state => state.deaths)
     const { episodes } = useAppSelector(state => state.episodes)
-    const arr = [deaths, episodes]
 
 
 
@@ -17,16 +20,26 @@ const SearchPage: React.FC = () => {
     const inputChange = (text: string) => {
         setInput(text)
         console.log(input);
+        const data = deaths.filter(deaths => deaths.last_words.includes(input))
+        setFiltredDeath(data)
+        const episodeData = episodes.filter(episodes => episodes.title.includes(input))
+        setFiltredEpisodes(episodeData)
+
     }
     useEffect(() => {
         dispatch(getAllEpisodes())
         dispatch(GetAllDeaths())
-        console.log(arr);
-
     }, [])
     return (
         <div className={s.folder}>
             <input value={input} onChange={(e) => inputChange(e.target.value)} type="text" name="" id="" />
+            {
+                input.length != 0 ? (filtredDeath.map(e => <p>Death: {e.last_words}</p>)) : (deaths.map(e => <p>  Death: {e.last_words}</p>))
+            }
+             {
+                input.length != 0 ? (filtredEpisodes.map(e => <p> Episode {e.title}</p>)) : (episodes.map(e => <p> Episode {e.title}</p>))
+            }
+
         </div>
     )
 }
